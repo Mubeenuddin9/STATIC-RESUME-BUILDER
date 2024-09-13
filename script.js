a6b032b6a72e931bb0116409866928a56e20b395
Mubeen Udidn <164794491+Mubeenuddin9@users.noreply.github.com>
2024-09-14 01:50:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var toggleButton = document.getElementById('toggleSkills');
+    var skillsSection = document.querySelector('.skills');
+    toggleButton.addEventListener('click', function () {
+        if (skillsSection.style.display === 'none') {
+            skillsSection.style.display = 'block';
+            toggleButton.textContent = 'Hide Skills';
+        }
+        else {
+            skillsSection.style.display = 'none';
+            toggleButton.textContent = 'Show Skills';
+        }
+    });
+});
+
+
+
+
+
+
